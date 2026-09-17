492de6e8f93069150a73f8e49fdcbec34b7e7f04
yacnuzun <yacn.uzun@gmail.com>
2026-09-17 19:56:19 +0300

    feat: add FluentValidation as a MediatR pipeline behavior

---
+++ src/ProductCqrsDemo.Api/Modules/Products/Application/Validators/CreateProductCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace ProductCqrsDemo.Api.Modules.Products.Application.Validators
+{
+    public sealed class CreateProductCommandValidator
+    : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Ürün adı zorunludur.")
+                .MaximumLength(200);
+
+            RuleFor(x => x.Description)
+                .MaximumLength(1000);
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Fiyat sıfırdan büyük olmalıdır.");
+
+            RuleFor(x => x.Stock)
+                .GreaterThanOrEqualTo(0).WithMessage("Stok negatif olamaz.");
+        }
+    }
+}
